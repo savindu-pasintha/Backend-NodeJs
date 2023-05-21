eeaec9cc74f129e0c07d9ad89122c68419c7600b
Savindu@Pasintha <64083148+savindu-pasintha@users.noreply.github.com>
2023-05-22 00:11:53 +0530

    Update homeRouter.js

--- routers/homeRouter.js
+++ routers/homeRouter.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { homeController } from '../controllers/HomeController.js';
+import { homeController } from '../controllers/homeController.js';
 
 const homeRouter = express.Router();
 
